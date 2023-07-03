5447a7672c9709896a6946f672d4a0977d04bd86
Leszek Lewandowski <leszek.lewandowski@dnv.com>
2023-07-03 13:59:06 +0200

    Kalkulator stage 1

--- ComarchCwiczenia/ComarchCwiczenia/Program.cs
+++ ComarchCwiczenia/ComarchCwiczenia/Program.cs
@@ -1,12 +1,58 @@
-﻿using System;
+﻿/*
+ Author: Leszek Lewandowski
+ Revision: 0.1
+ */
+using System;
 
 namespace ComarchCwiczenia
 {
-    internal class Program
+    /// <summary>
+    /// Główna klasa aplikacji
+    /// </summary>
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("KALKULATOR 1.0");
+
+            Console.WriteLine(" 1. Dodawanie");
+            Console.WriteLine(" 2. Odejmowanie");
+            Console.WriteLine(" 3. Mnożenie");
+            Console.WriteLine(" 4. Dzielenie");
+            Console.WriteLine(" 5. Modulo");
+
+            Console.Write("Podaj pozycję menu: ");
+
+            if (int.TryParse(Console.ReadLine(), out int wybor) == true)
+            {
+                Console.Write("Podaj x: ");
+                int x = int.Parse(Console.ReadLine());
+                Console.Write("Podaj y: ");
+                int y = int.Parse(Console.ReadLine());
+
+                switch (wybor)
+                {
+                    case 1:
+                        Console.WriteLine($"Wynik dodawania {x} oraz {y} to {x + y}");
+                        break;
+                    case 2:
+                        Console.WriteLine($"Wynik odejmowania {x} oraz {y} to {x - y}");
+                        break;
+                    case 3:
+                        Console.WriteLine($"Wynik mnożenia {x} oraz {y} to {x * y}");
+                        break;
+                    case 4:
+                        Console.WriteLine($"Wynik dzielenia {x} oraz {y} to {x / y}");
+                        break;
+                    case 5:
+                        Console.WriteLine($"Wynik reszty z dzielenia {x} oraz {y} to {x % y}");
+                        break;
+                    default:
+                        Console.WriteLine("Nieprawidłowy wybór.");
+                        break;
+                }
+
+            }
         }
     }
 }
