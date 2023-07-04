0e038e80d8b97e6e32ae11abca114b3861a21505
Leszek Lewandowski <leszek.lewandowski@dnv.com>
2023-07-04 16:14:46 +0200

    Model danych i binding do DataGrida

--- Bibliotekarz/Bibliotekarz.App/MainWindow.xaml.cs
+++ Bibliotekarz/Bibliotekarz.App/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Bibliotekarz.Model.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,56 @@ namespace Bibliotekarz.App
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+
+            GenerateFakeData();
+        }
+
+        public ObservableCollection<Book> BookList { get; set; } = new();
+
+        private void GenerateFakeData()
+        {
+            Book book = new Book
+            {
+                Id = 1,
+                Author = "Leszek Lewandowski",
+                Title = "Programowanie w C#",
+                PageCount = 234,
+                IsBorrowed = false
+            };
+            BookList.Add(book);
+
+            book = new Book
+            {
+                Id = 1,
+                Author = "John Sharp",
+                Title = "Visual Studio krok po kroku",
+                PageCount = 800,
+                IsBorrowed = true,
+                Borrower = new Customer
+                {
+                    Id = 1,
+                    FirstName = "Jan",
+                    LastName = "Kowalski"
+                }
+            };
+            BookList.Add(book);
+        }
+
+        private void OnCloseClick(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Czy chcesz zamnknąć aplikację?", "Zamykanie aplikacji", 
+                MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                Environment.Exit(0);            
+            }
+            else
+            {
+                e.Handled = true;
+            }
+
         }
     }
 }
