c6ca103ae63bfbc65f6fdfa73b11916a796119d2
Leszek Lewandowski <leszek.lewandowski@dnv.com>
2023-07-05 11:14:31 +0200

    Migracje EF Core Wygląd datagrid

--- Bibliotekarz/Bibliotekarz.Model/Migrations/20230705084101_ALterCustomerAddAgeColumn.Designer.cs
+++ Bibliotekarz/Bibliotekarz.Model/Migrations/20230705084101_ALterCustomerAddAgeColumn.Designer.cs
@@ -3,6 +3,7 @@ using Bibliotekarz.Model.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Bibliotekarz.Model.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230705084101_ALterCustomerAddAgeColumn")]
+    partial class ALterCustomerAddAgeColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Bibliotekarz/Bibliotekarz.Model/Migrations/20230705084101_ALterCustomerAddAgeColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bibliotekarz.Model.Migrations
+{
+    /// <inheritdoc />
+    public partial class ALterCustomerAddAgeColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "Borrowers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "Borrowers");
+        }
+    }
+}
